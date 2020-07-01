5f16c8f6463edf5ee5b89dd9cf93b0f3a5e1a7c5
dsuru1499 <dsuru5244@gmail.com>
2020-07-01 11:45:19 +0200

    Ajout : local maven repository

--- src/main/java/siri_xlite/common/PurgeService.java
+++ src/main/java/siri_xlite/common/PurgeService.java
@@ -14,8 +14,25 @@ import java.time.Duration;
 public class PurgeService {
 
     private static final String PURGE = "PURGE";
+    private static final String DELETE = "DELETE";
+    private static final String NAME = "NAME";
+
     private static final HttpClient client = HttpClient.newHttpClient();
     private static final HttpRequest.Builder builder = HttpRequest.newBuilder();
+
+    public boolean delete(String uri, String rule) {
+        boolean result = false;
+
+        try {
+            HttpRequest request = builder.uri(URI.create(uri)).method(DELETE, HttpRequest.BodyPublishers.noBody())
+                    .timeout(Duration.ofSeconds(1)).header(NAME, rule).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            result = (response.statusCode() == 200);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return result;
+    }
 
     public boolean purge(String uri) {
         boolean result = false;
